66b40c20db901680205f47695eb661dd87d42b05
alexalins <alexa@glinsoo.com>
2022-06-28 12:39:20 -0300

    pokemon adapter

--- app/src/main/java/com/alexa/mypokemonbag/adapter/PokemonAdapter.java
+++ app/src/main/java/com/alexa/mypokemonbag/adapter/PokemonAdapter.java
@@ -11,16 +11,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.alexa.mypokemonbag.R;
-import com.alexa.mypokemonbag.model.Bag;
-import com.alexa.mypokemonbag.model.Region;
+import com.alexa.mypokemonbag.model.Pokemon;
 
 import java.util.List;
 
-public class RegionAdapter extends ArrayAdapter<Region> {
+public class PokemonAdapter extends ArrayAdapter<Pokemon> {
 
-    private List<Region> list;
+    private List<Pokemon> list;
 
-    public RegionAdapter(@NonNull Context context, @NonNull List<Region> objects) {
+    public PokemonAdapter(@NonNull Context context, @NonNull List<Pokemon> objects) {
         super(context, 0, objects);
         this.list = objects;
     }
@@ -28,13 +27,13 @@ public class RegionAdapter extends ArrayAdapter<Region> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        Region region = getItem(position);
+        Pokemon pokemon = getItem(position);
         if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_region, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_pokemon, parent, false);
         }
         //
-        TextView txtTitle = convertView.findViewById(R.id.region_name);
-        txtTitle.setText(region.getName());
+        TextView txtTitle = convertView.findViewById(R.id.pokemon_name);
+        txtTitle.setText(pokemon.getName());
         return convertView;
     }
 }
